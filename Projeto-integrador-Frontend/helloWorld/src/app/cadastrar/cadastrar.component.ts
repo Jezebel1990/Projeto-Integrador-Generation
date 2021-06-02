b0d7c3a600ccb4f9f4c159cdcde784113891fda1
Maicon <hrqn.fernandes@gmail.com>
2021-06-02 11:16:11 -0300

    implementado consumo de api de login e cadastro

--- Projeto-integrador-Frontend/helloWorld/src/app/cadastrar/cadastrar.component.ts
+++ Projeto-integrador-Frontend/helloWorld/src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../Model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css']
 })
+
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmarSenha: string
 
-  ngOnInit(): void {
+  constructor() { }
+private authService: AuthService
+private router: Router
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+confirmSenha(event:any){
+  this.confirmarSenha = event.target.value
+}
+
+cadastrar(){
+  if(this.user.senha != this.confirmarSenha){
+    alert('Senhas Diferentes!')
+  }else{
+    this.authService.cadastrar(this.user).subscribe((resp: User) =>{
+      this.user = resp
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso!') 
+    })
+  }
+}
 
 }
