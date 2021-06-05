ba6e537c0a9e12a0398efb4428d7b1d8d27505f5
Jezebel1990 <jezebelguedes@gmail.com>
2021-06-05 02:14:03 -0300

    Implementação do menu e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
